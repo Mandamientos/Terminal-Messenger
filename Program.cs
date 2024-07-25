7797d4656087ac6c6961cc78d4cb4541e37f4e11
Guillermo <guillermosdlg@gmail.com>
2024-07-25 17:02:15 -0600

    Ingresar puerto a escuchar y enviar mensajes.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class TerminalMessenger {
+    public static void Main(string[] args){
+
+        Console.Clear();
+        Int32 targetPort = 0;
+
+        while (targetPort == 0 ){
+
+            Console.Write("Ingrese el puerto con el cual se quiere comunicar > ");
+
+            string targetPortStr = Console.ReadLine();
+
+            try {
+                targetPort = Int32.Parse(targetPortStr);
+                if (49152 > targetPort || targetPort > 65535){
+                    Console.WriteLine("El puerto debe estar en el rango de 49152-65535.");
+                    targetPort = 0;
+                    continue;
+                }
+            } catch (Exception ex) {
+                Console.WriteLine("¡Eso no es un puerto!");
+            }
+
+
+        }
+
+
+
+    }
+}
